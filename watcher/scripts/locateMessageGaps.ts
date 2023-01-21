4ecb16ed57d4175619f011bb70382815a9663c95
Evan Gray <battledingo@gmail.com>
2023-01-21 05:59:25 +0000

    scripts: improve locate gaps

--- watcher/scripts/locateMessageGaps.ts
+++ watcher/scripts/locateMessageGaps.ts
@@ -1,7 +1,10 @@
 import * as dotenv from 'dotenv';
 dotenv.config();
 import { ChainId, coalesceChainName } from '@certusone/wormhole-sdk';
-import { sleep } from '@wormhole-foundation/wormhole-monitor-common';
+import {
+  INITIAL_DEPLOYMENT_BLOCK_BY_CHAIN,
+  sleep,
+} from '@wormhole-foundation/wormhole-monitor-common';
 import { TIMEOUT } from '../src/consts';
 import { BigtableDatabase } from '../src/databases/BigtableDatabase';
 import { parseMessageId } from '../src/databases/utils';
@@ -35,9 +38,13 @@ import { Watcher } from '../src/watchers/Watcher';
         emitter: emitterAddress,
         sequence,
       } = parseMessageId(observedMessage.id);
+      const chainName = coalesceChainName(emitterChain as ChainId);
       const emitter = `${emitterChain}/${emitterAddress}`;
       if (!latestEmission[emitter]) {
-        latestEmission[emitter] = { sequence: 0n, block: 0 };
+        latestEmission[emitter] = {
+          sequence: chainName === 'algorand' || chainName === 'near' ? 0n : -1n,
+          block: Number(INITIAL_DEPLOYMENT_BLOCK_BY_CHAIN[chainName] || 0),
+        };
       }
       while (sequence > latestEmission[emitter].sequence + 1n) {
         latestEmission[emitter].sequence += 1n;
@@ -70,6 +77,10 @@ import { Watcher } from '../src/watchers/Watcher';
     for (const gap of gaps) {
       const [chain, blockRange, emitter, sequence] = gap.split('/');
       const chainName = coalesceChainName(Number(chain) as ChainId);
+      if (chainName === 'algorand') {
+        console.warn('skipping algorand gaps until backfilled');
+        continue;
+      }
       let watcher: Watcher;
       try {
         watcher = makeFinalizedWatcher(chainName);
